0b7b7427aede83976462996dad5bc8ebf1b4e99e
Евгений Тюлькин <114156337+Jekeika@users.noreply.github.com>
2023-12-18 20:42:35 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const slides = document.querySelector('.cards_slider');
+    const prevBtn = document.querySelector('.button_left');
+    const nextBtn = document.querySelector('.button_right');
+    const slideWidth = document.querySelector('.card_slider').clientWidth;
+    let slideIndex = 0;
+
+    prevBtn.addEventListener('click', function() {
+        slideIndex = Math.max(slideIndex - 1, 0);
+        slides.style.transform = `translateX(-${slideIndex * slideWidth}px)`
+        
+    });
+
+    nextBtn.addEventListener('click', function() {
+        slideIndex = Math.min(slideIndex + 1, slides.children.length - 1);
+        slides.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
+    });
+});
+
+// Скрипт для
+const menuButton = document.querySelector(".m-menu_button");
+const menu = document.querySelector(".menu")
+menuButton.addEventListener('click', function() {
+    console.log('Кликнули по меню');
+    menu.classList.toggle("is-open");
+});
